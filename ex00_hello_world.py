d5a2406144c095522087ce5f905290365a8c2f41
ececilyh <ececilyh@unc.edu>
2025-02-12 10:24:43 -0500

    A V-Day Game!

--- ex00_hello_world.py
+++ ex00_hello_world.py
@@ -10,3 +10,9 @@ def greet(name: str) -> str:
 
 if __name__ == "__main__":
     print(greet(name=input("What is your name?")))
+
+
+def guess_secret(word: str, secret, str, idx: int = 0) -> bool:
+
+    print(guess_secret(word=input("What is your word? "), secret=SECRET))
+    print(guess_secret(word=input("What is your word? "), secret=SECRET, idx=2))
